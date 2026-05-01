2822a34226c8e0f186228c258c73c51386b8abd7
claracanyis <claraccc9@gmail.com>
2026-05-01 20:57:02 +0200

    Returns random computer choice

---
+++ rock-paper-scissors.js
@@ -0,0 +1,21 @@
+// Create random integer from 0 to (max - 1):
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+// Get computer's choice
+function getComputerChoice() {
+    let choice = getRandomInt(3);
+    switch (choice) {
+        case 0:
+            return 'rock';
+        case 1:
+            return 'paper';
+        case 2:
+            return 'scissors';
+        default:
+            return 'something went wrong';
+    }
+}
+
+console.log(getComputerChoice());
